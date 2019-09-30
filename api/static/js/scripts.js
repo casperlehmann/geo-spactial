e136a18127450093cf6d6d16eee20bbee357bcec
Casper Lehmann <casperlehmann@gmail.com>
2019-10-01 00:57:28 +0200

    Expand user interface in web. Bring example uses and clear.

--- api/static/js/scripts.js
+++ api/static/js/scripts.js
@@ -15,3 +15,39 @@ function make_request() {
   XHR.open('POST', '/geo_code');
   XHR.send(FD);
 }
+
+function random_choice(choices) {
+  let chosen_index = Math.floor(Math.random()*choices.length);
+  return choices[chosen_index];
+}
+
+function clear_fields() {
+  document.getElementById('address').value = '';
+  document.getElementById('lat').value = '';
+  document.getElementById('lng').value = '';
+  document.getElementById('response').value = '';
+}
+
+function example() {
+  clear_fields();
+  if(Math.random() > .5) {
+      document.getElementById('address').value = random_choice([
+          'New York City',
+          'London',
+          'Copenhagen',
+          'Istanbul',
+          'Beijing',
+          'Tokyo',
+          'H. C. Andersens Blvd. 27, 1553 København, Denmark',
+          'H.C. Andersens Boulevard 27, 1553 København, Danmark'
+      ]);
+  } else {
+      [(document.getElementById('lat').value), (document.getElementById('lng').value)] = random_choice([
+          [40.7127281, -74.0060152],
+          [55.674136, 12.571782],
+          [41.0096334, 28.9651646],
+          [39.906217, 116.3912757],
+          [35.6828387,139.7594549]
+      ]);
+  }
+}
